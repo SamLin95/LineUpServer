c31f538976ba78a7f2ee527016750e9807ee52bf
slin77 <slin77@gatech.edu>
2015-07-01 00:09:02 -0400

    add first blueprint

--- application/__init__.py
+++ application/__init__.py
@@ -1,10 +1,16 @@
-from flask import Flask
-from monogengine import connect, register_connection
+from flask import Flask, request
+from mongoengine import connect, register_connection
 
 app = Flask(__name__)
 app.config.from_object('config')
+connect(app.config['DATABASE'])
 
-connect(app.config['MONGO_DATABASE'])
+from mod_auth.controllers import mod_auth
+app.register_blueprint(mod_auth)
+
+@app.route("/", methods=["GET"])
+def root():
+	return "hello world", 200
 
 
 
---
+++ application/mod_auth/controllers.py
@@ -0,0 +1,3 @@
+from flask import Blueprint
+
+mod_auth = Blueprint("auth", __name__, url_prefix="/auth")
