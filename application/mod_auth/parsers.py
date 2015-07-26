ce697e15375e2186752fe9e576633717e3ef83a7
Sizhe <samlin950205@gmail.com>
2015-07-26 01:40:54 -0400

    fix some bugs

--- application/mod_auth/parsers.py
+++ application/mod_auth/parsers.py
@@ -6,8 +6,8 @@ auth_parser.add_argument("username", type=str)
 auth_parser.add_argument("password", type=str)
 
 sign_up_parser = reqparse.RequestParser()
-sign_up_parser.add_argument("username", type=str, required=True)
-sign_up_parser.add_argument("password", type=str, required=True)
-sign_up_parser.add_argument("email", type=str, required=True)
-sign_up_parser.add_argument("name", type=str, required=True)
+sign_up_parser.add_argument("username", type=str, required=True, help="username")
+sign_up_parser.add_argument("password", type=str, required=True, help="password")
+sign_up_parser.add_argument("email", type=str, required=True, help="Your Email")
+sign_up_parser.add_argument("name", type=str, required=True, help="fuck you")
 
