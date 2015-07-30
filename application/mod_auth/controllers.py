a2d95eeee791e7ce97e88bd040b719055f571395
ywang751 <ywang751@gatech.edu>
2015-07-29 23:26:28 -0400

    Editting signup validation

--- application/mod_auth/controllers.py
+++ application/mod_auth/controllers.py
@@ -48,7 +48,7 @@ def signup():
         new_user.save()
         return "signed up", 200
     except NotUniqueError as e:
-        return redirect(url_for(signup))
+        return redirect(url_for(login))
 
 @mod_auth.route("/logout", methods=["POST"])
 def logout():
